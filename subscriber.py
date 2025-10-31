9fd410c0917ba98f79d9d975cdeb3aa2f2451e72
Erik <jaramilloerik21@gmail.com>
2025-10-31 20:31:31 +0100

    refactor: adapt subscriber parent class to event object argument in "receive" function.

--- subscriber.py
+++ subscriber.py
@@ -1,5 +1,5 @@
 from broker import PubSubBroker
 
 class Subscriber:
-    def receive(self, message):
+    def receive(self, event):
         raise NotImplementedError("Subscribers must implement their own receive() method")
